383f1d824713fc358e8999ea92de206e15504e1d
Dushyant Sirohi <dushyantsirohi217@gmail.com>
2023-09-14 13:34:24 -0400

    auth functionality

--- app/http/controllers/authController.js
+++ app/http/controllers/authController.js
@@ -1,10 +1,83 @@
+const User = require("../../models/user");
+const bcrypt = require("bcrypt");
+const passport = require("passport");
 function authController() {
   return {
     login(req, res) {
       res.render("auth/login");
     },
+    postLogin(req, res, next) {
+      const { email, password } = req.body;
+      if (!email || !password) {
+        req.flash("error", "All fields are required");
+        req.flash("email", email);
+        return res.redirect("/login");
+      }
+
+      passport.authenticate("local", (error, user, info) => {
+        if (error) {
+          req.flash("error", info.message);
+          return next(error);
+        }
+        if (!user) {
+          req.flash("error", info.message);
+          return res.redirect("/login");
+        }
+        req.logIn(user, (err) => {
+          if (err) {
+            req.flash("error", info.message);
+            return next(err);
+          }
+          return res.redirect("/");
+        });
+      })(req, res, next);
+    },
     register(req, res) {
       res.render("auth/register");
+    },
+    async registerUser(req, res) {
+      const { name, email, password } = req.body;
+      req.flash("error", "All fields are required");
+      if (!name || !email || !password) {
+        req.flash("name", name);
+        req.flash("email", email);
+        return res.redirect("/register");
+      }
+
+      // Check Email Unique
+
+      const result = await User.exists({ email: email });
+      if (result) {
+        req.flash("error", "Email already exists");
+        req.flash("name", name);
+        req.flash("email", email);
+        return res.redirect("/register");
+      }
+
+      const hashedPassword = await bcrypt.hash(password, 10);
+      const userData = new User({
+        name: name,
+        email: email,
+        password: hashedPassword,
+      });
+      userData
+        .save()
+        .then((user) => {
+          return res.redirect("/");
+        })
+        .catch((err) => {
+          req.flash("error", "Something went wrong");
+          return res.redirect("/register");
+        });
+    },
+    logoutUser(req, res) {
+      req.logout(function (err) {
+        if (err) {
+          return next(err);
+        }
+        return res.redirect("/");
+      });
+      // return res.redirect("/");
     },
   };
 }
--- server.js
+++ server.js
@@ -9,6 +9,7 @@ const mongoose = require("mongoose");
 const session = require("express-session");
 const flash = require("express-flash");
 const MongoStore = require("connect-mongo");
+const passport = require("passport");
 const PORT = process.env.PORT || 3000;
 const DATABASE_URI = process.env.DATABASE_URI || "mongodb://localhost:27017";
 
@@ -34,13 +35,21 @@ app.use(
   })
 );
 
+// Passport auth
+const passportInit = require("./app/config/passport");
+app.use(passport.initialize());
+app.use(passport.session());
+passportInit(passport);
+
 app.use(flash());
 
 app.use(expressLayout);
 app.use(express.static("public"));
+app.use(express.urlencoded());
 app.use(express.json());
 app.use((req, res, next) => {
   res.locals.session = req.session;
+  res.locals.user = req.user;
   next();
 });
 app.set("views", path.join(__dirname, "/resources/views"));
